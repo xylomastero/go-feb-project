4ed8047b7318fa662bdbcc3481f0e5b3f26a5535
xylomastero <xylomastero@gmail.com>
2025-03-05 03:46:16 -0600

    api update

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ func Start() {
 
 	router := routes.SetupRoutes()
 
-	port := ":8080" // or whatever port you intend to use
+	port := ":8080"
 	fmt.Printf("Starting server on port %s\n", port)
 	err := http.ListenAndServe(port, router)
 	if err != nil {
